485aa6f4b54081b31e5807fb8526aef14abf073f
Joao Rafael <joaorafael1344@gmail.com>
2026-05-06 16:51:07 -0300

    envio de arquivos iniciais

---
+++ src/main/java/Infnet/TP_Assessment/TpAssessmentApplication.java
@@ -0,0 +1,13 @@
+package Infnet.TP_Assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Infnet/TP_Assessment/TpAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package Infnet.TP_Assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
